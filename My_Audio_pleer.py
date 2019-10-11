6875cab4d8e83c0e115270a1903df2dafab2a356
AlexHryb <alexgrib@gmail.com>
2019-10-11 16:15:00 +0300

    work 2019 10 11 Player

--- My_Audio_pleer.py
+++ My_Audio_pleer.py
@@ -19,23 +19,102 @@ class MyWindow(QtWidgets.QWidget):
         #Комопоненты управления медиа
         self.sldPosition = QtWidgets.QSlider(QtCore.Qt.Horizontal)
         self.sldPosition.setMinimum(0)
-        self.sldPosition.valueChanged.connect.(self.mplPlayer.setPosition)
-        self.mplPlayer.positionChanged.connect.(self.sldPosition.setValue)
+        self.sldPosition.valueChanged.connect(self.mplPlayer.setPosition)
+        self.mplPlayer.positionChanged.connect(self.sldPosition.setValue)
+        self.mplPlayer.mediaStatusChanged.connect(self.showMetadata)
+        self.sldPosition.setEnabled(False)
+        vbox.addWidget(self.sldPosition)
+
+        hbox = QtWidgets.QHBoxLayout()
+
+        self.btnPlay = QtWidgets.QPushButton('&Play')
+        self.btnPlay.clicked.connect(self.mplPlayer.play)
+        self.btnPlay.setEnabled(False)
+        hbox.addWidget(self.btnPlay)
+        self.btnPause = QtWidgets.QPushButton('P&ause')
+        self.btnPause.clicked.connect(self.mplPlayer.pause)
+        self.btnPause.setEnabled(False)
+        hbox.addWidget(self.btnPause)
+        self.btnStop = QtWidgets.QPushButton('&Stop')
+        self.btnStop.clicked.connect(self.mplPlayer.stop)
+        self.btnStop.setEnabled(False)
+        hbox.addWidget(self.btnStop)
+        vbox.addLayout(hbox)
+        # volume control
+        hbox = QtWidgets.QHBoxLayout()
+        lblVolume = QtWidgets.QLabel('&Volume')
+        hbox.addWidget(lblVolume)
+        sldVolume = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        sldVolume.setRange(0, 100)
+        sldVolume.setTickPosition(QtWidgets.QSlider.TicksAbove)
+        sldVolume.setTickInterval(10)
+        lblVolume.setBuddy(sldVolume)
+        sldVolume.valueChanged.connect(self.mplPlayer.setVolume)
+        hbox.addWidget(sldVolume)
+        btnMute = QtWidgets.QPushButton('&Mute')
+        btnMute.setCheckable(True)
+        btnMute.toggled.connect(self.mplPlayer.setMuted)
+        hbox.addWidget(btnMute)
+        vbox.addLayout(hbox)
+
+        self.txtOutput = QtWidgets.QTextEdit()
+        self.txtOutput.setReadOnly(True)
+        vbox.addWidget(self.txtOutput)
 
         #main vbox
         self.setLayout(vbox)
-        self.resize(400, 150)
+        self.resize(800, 300)
+
+    def showMetadata(self, state):
+        self.txtOutput.clear()
 
     def openFile(self):
-        return
+        file = QtWidgets.QFileDialog.getOpenFileUrl(parent=self, caption='Select audio file', filter='Audio files (*.mp3 *.ac3)')
+        self.mplPlayer.setMedia(QtMultimedia.QMediaContent(file[0]))
 
     def initPlayer(self, state):
-        return
+        if state == QtMultimedia.QMediaPlayer.LoadedMedia:
+
+            self.mplPlayer.stop()
+            self.btnPlay.setEnabled(True)
+            self.btnPause.setEnabled(False)
+            self.sldPosition.setEnabled(True)
+            self.sldPosition.setMaximum(self.mplPlayer.duration())
+        elif state == QtMultimedia.QMediaPlayer.EndOfMedia:
+
+            self.mplPlayer.stop()
+            self.sldPosition.setValue(0)
+            self.sldPosition.setEnabled(False)
+            self.btnPlay.setEnabled(False)
+            self.btnPause.setEnabled(False)
+            self.btnStop.setEnabled(False)
+        elif state == QtMultimedia.QMediaPlayer.NoMedia or state == QtMultimedia.QMediaPlayer.InvalidMedia:
+
+            self.sldPosition.setValue(0)
+            self.sldPosition.setEnabled(False)
+            self.btnPlay.setEnabled(False)
+            self.btnPause.setEnabled(False)
+            self.btnStop.setEnabled(False)
 
     def setPlayerState(self, state):
-        return
+        if state == QtMultimedia.QMediaPlayer.StoppedState:
 
-if __name__ == '__main__'
+            self.sldPosition.setValue(0)
+            self.btnPlay.setEnabled(True)
+            self.btnPause.setEnabled(False)
+            self.btnStop.setEnabled(False)
+        elif state == QtMultimedia.QMediaPlayer.PlayingState:
+
+            self.btnPlay.setEnabled(False)
+            self.btnPause.setEnabled(True)
+            self.btnStop.setEnabled(True)
+        elif state == QtMultimedia.QMediaPlayer.PausedState:
+
+            self.btnPlay.setEnabled(True)
+            self.btnPause.setEnabled(False)
+            self.btnStop.setEnabled(True)
+
+if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow()
     window.show()
---
+++ test.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'D:\Project\PyQt_5_Begin\test.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.lineEdit = QtWidgets.QLineEdit(Form)
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout.addWidget(self.lineEdit)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout.addWidget(self.label_2)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout_2.addWidget(self.pushButton_2)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "TextLabel"))
+        self.label_2.setText(_translate("Form", "TextLabel"))
+        self.pushButton_2.setText(_translate("Form", "PushButton"))
+        self.pushButton.setText(_translate("Form", "PushButton"))
